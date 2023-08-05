4b2dca30230cf615be6e78abd1ed899adf9089e4
Jubast <30406814+Jubast@users.noreply.github.com>
2023-08-05 18:45:53 +0000

    use image platform for gifs

--- custom_components/noaa_solar/__init__.py
+++ custom_components/noaa_solar/__init__.py
@@ -1,24 +1,24 @@
 """The NOAA Solar integration."""
 from __future__ import annotations
 import logging
-
+from datetime import timedelta
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import Platform, CONF_HOST, CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 
+from .api import NOAASpaceApi
 from .coordinator import (
     NOAASolarActivityUpdateCoordinator,
     NOAASolarMagFieldUpdateCoordinator,
     NOAASolarUpdateCoordinator,
     NOAASolarWindSpeedUpdateCoordinator,
-    NOAASolarLascoC3UpdateCoordinator,
     NOAASolarSuvi304UpdateCoordinator,
-    NOAASpaceApi,
+    NOAASolarLascoC3UpdateCoordinator,
 )
 
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.IMAGE]
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -26,14 +26,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Solar System Scraper from a config entry."""
     _LOGGER.info("Setup NOAA Space API Coordinators")
 
-    api = NOAASpaceApi(entry)
+    api_host = entry.data[CONF_HOST]
+    api_interval = timedelta(seconds=entry.data[CONF_SCAN_INTERVAL])
+
+    api = NOAASpaceApi(api_host)
 
     coordinators: dict[str, NOAASolarUpdateCoordinator] = {
-        "mag_field": NOAASolarMagFieldUpdateCoordinator(hass, entry, api),
-        "wind_speed": NOAASolarWindSpeedUpdateCoordinator(hass, entry, api),
-        "activity": NOAASolarActivityUpdateCoordinator(hass, entry, api),
-        "suvi_304": NOAASolarSuvi304UpdateCoordinator(hass, entry, api),
-        "lasco_c3": NOAASolarLascoC3UpdateCoordinator(hass, entry, api),
+        "mag_field": NOAASolarMagFieldUpdateCoordinator(hass, api_interval, api),
+        "wind_speed": NOAASolarWindSpeedUpdateCoordinator(hass, api_interval, api),
+        "activity": NOAASolarActivityUpdateCoordinator(hass, api_interval, api),
+        "suvi_304": NOAASolarSuvi304UpdateCoordinator(hass, api_interval, api),
+        "lasco_c3": NOAASolarLascoC3UpdateCoordinator(hass, api_interval, api),
     }
 
     for coordinator in coordinators.values():
--- custom_components/noaa_solar/utils/gif_utils.py
+++ custom_components/noaa_solar/utils/gif_utils.py
@@ -12,15 +12,35 @@ from datetime import datetime
 from PIL import Image
 
 
-def save_png_gif_frame(image: bytes, image_directory: str) -> bool:
+class Gif:
+    """GIF object."""
+
+    def __init__(self, data: bytes, created: datetime) -> None:
+        """Initializes the GIF."""
+        self.data = data
+        self.created = created
+
+
+class GifFrameRef:
+    """GIF frame ref object."""
+
+    def __init__(self, file_name: str, file_datetime: datetime, saved: bool) -> None:
+        """Initializes the GIF frame ref."""
+        self.file_name = file_name
+        self.file_datetime = file_datetime
+        self.saved = saved
+
+
+def save_png_gif_frame(image: bytes, image_directory: str) -> GifFrameRef:
     """Save a png image to the file system if it doesn't already exist."""
 
     _ensure_directory_exists(image_directory)
-    saved = _save_image_if_not_exists(image_directory, image)
-    if saved:
+    gif_frame = _save_image_if_not_exists(image_directory, image)
+
+    if gif_frame.saved:
         _remove_excess_images(image_directory)
 
-    return saved
+    return gif_frame
 
 
 def create_gif(image_directory: str) -> bytes:
@@ -59,30 +79,31 @@ def save_gif(directory: str, gif_name: str, data: bytes) -> None:
         file.write(data)
 
 
-def _save_image_if_not_exists(directory: str, data: bytes) -> bool:
-    hash_result = sha1(data).hexdigest()
-
-    glob_path = join(directory, hash_result + "*.png")
+def _save_image_if_not_exists(directory: str, data: bytes) -> GifFrameRef:
+    image_hash = sha1(data).hexdigest()
+    glob_path = join(directory, image_hash + "*.png")
     glob_paths = glob(glob_path)
 
     if glob_paths:
-        return False
+        file_name = glob_paths[0]
+        file_datetime = _get_datetime_from_filename(file_name)
+        return GifFrameRef(file_name, file_datetime, False)
 
-    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-    image_name = hash_result + "_" + current_datetime + ".png"
+    current_datetime = datetime.now()
+    image_name = image_hash + "_" + current_datetime.strftime("%Y%m%d%H%M%S") + ".png"
 
-    file_name = join(directory, image_name)
-    with open(file_name, "wb") as file:
+    file_path = join(directory, image_name)
+    with open(file_path, "wb") as file:
         file.write(data)
 
-    return True
+    return GifFrameRef(image_name, current_datetime, True)
 
 
 def _ensure_directory_exists(directory: str) -> None:
     makedirs(directory, exist_ok=True)
 
 
-def _get_datetime_from_filename(file_path: str):
+def _get_datetime_from_filename(file_path: str) -> datetime:
     file_name = basename(file_path)
     datetime_string = file_name.split("_")[1].split(".")[0]
     return datetime.strptime(datetime_string, "%Y%m%d%H%M%S")
@@ -93,7 +114,7 @@ def _remove_excess_images(directory: str) -> None:
     glob_paths = glob(glob_path)
     sorted_glob_paths = sorted(glob_paths, key=_get_datetime_from_filename)
 
-    max_images = 30
+    max_images = 60
     if len(glob_paths) >= max_images:
         excess_count = len(sorted_glob_paths) - max_images
 
