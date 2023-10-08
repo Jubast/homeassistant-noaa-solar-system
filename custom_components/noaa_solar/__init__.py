5c14aca1f1f04e93bcb4242d6b2def5e1dc0b190
Jubast <30406814+Jubast@users.noreply.github.com>
2023-10-08 17:40:43 +0000

    added separate scan interval for images

--- custom_components/noaa_solar/__init__.py
+++ custom_components/noaa_solar/__init__.py
@@ -16,27 +16,33 @@ from .coordinator import (
     NOAASolarLascoC3UpdateCoordinator,
 )
 
-from .const import DOMAIN
+from .const import (
+    DOMAIN,
+    CONF_DATA_SCAN_INTERVAL,
+    CONF_IMAGE_SCAN_INTERVAL,
+    DEFAULT_IMAGE_SCAN_INTERVAL,
+)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.IMAGE]
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Solar System Scraper from a config entry."""
+    """Set up NOAA Solar from a config entry."""
     _LOGGER.info("Setup NOAA Space API Coordinators")
 
     api_host = entry.data[CONF_HOST]
-    api_interval = timedelta(seconds=entry.data[CONF_SCAN_INTERVAL])
+    api_data_interval = timedelta(seconds=entry.data[CONF_DATA_SCAN_INTERVAL])
+    api_image_interval = timedelta(seconds=entry.data[CONF_IMAGE_SCAN_INTERVAL])
 
     api = NOAASpaceApi(api_host)
 
     coordinators: dict[str, NOAASolarUpdateCoordinator] = {
-        "mag_field": NOAASolarMagFieldUpdateCoordinator(hass, api_interval, api),
-        "wind_speed": NOAASolarWindSpeedUpdateCoordinator(hass, api_interval, api),
-        "activity": NOAASolarActivityUpdateCoordinator(hass, api_interval, api),
-        "suvi_304": NOAASolarSuvi304UpdateCoordinator(hass, api_interval, api),
-        "lasco_c3": NOAASolarLascoC3UpdateCoordinator(hass, api_interval, api),
+        "mag_field": NOAASolarMagFieldUpdateCoordinator(hass, api_data_interval, api),
+        "wind_speed": NOAASolarWindSpeedUpdateCoordinator(hass, api_data_interval, api),
+        "activity": NOAASolarActivityUpdateCoordinator(hass, api_data_interval, api),
+        "suvi_304": NOAASolarSuvi304UpdateCoordinator(hass, api_image_interval, api),
+        "lasco_c3": NOAASolarLascoC3UpdateCoordinator(hass, api_image_interval, api),
     }
 
     for coordinator in coordinators.values():
@@ -55,3 +61,19 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Migrations for config flow configuration."""
+
+    if config_entry.version == 1:
+        _LOGGER.info("Migrating NOAA Solar integration")
+
+        new_data = {**config_entry.data}
+        new_data[CONF_DATA_SCAN_INTERVAL] = config_entry.data[CONF_SCAN_INTERVAL]
+        new_data[CONF_IMAGE_SCAN_INTERVAL] = DEFAULT_IMAGE_SCAN_INTERVAL
+        new_data.pop(CONF_SCAN_INTERVAL)
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry, data=new_data)
+
+    return True
--- custom_components/noaa_solar/const.py
+++ custom_components/noaa_solar/const.py
@@ -1,8 +1,16 @@
 """Constants for the NOAA Solar integration."""
 
+from typing import Final
+
+
 DOMAIN = "noaa_solar"
 
 # Default config for solar system scraper.
 DEFAULT_HOST = "https://services.swpc.noaa.gov/"
 DEFAULT_NAME = "NOAA Solar"
-DEFAULT_SCAN_INTERVAL = 60  # seconds
+DEFAULT_DATA_SCAN_INTERVAL = 60  # seconds
+DEFAULT_IMAGE_SCAN_INTERVAL = 3600  # seconds
+
+# Configuration defaults
+CONF_DATA_SCAN_INTERVAL: Final = "data_scan_interval"
+CONF_IMAGE_SCAN_INTERVAL: Final = "image_scan_interval"
