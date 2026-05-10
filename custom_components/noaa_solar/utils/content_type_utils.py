050b4384478bd37ba8b3347a3dbd3db825af6190
Jubast <30406814+Jubast@users.noreply.github.com>
2026-05-10 19:06:33 +0000

    removed not used code

--- custom_components/noaa_solar/utils/content_type_utils.py
+++
@@ -1,9 +0,0 @@
-"""Content type utils for image platform."""
-
-
-def get_content_type(video_format: str) -> str:
-    """Returns content type for video format."""
-    if video_format == "MP4":
-        return "video/mp4"
-
-    return "image/gif"
